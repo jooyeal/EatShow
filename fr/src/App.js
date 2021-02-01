c7dcd0c136a743bb1b10c253f9221d52b9745094
lpaqkosw <jaehoonlee1994@gmail.com>
2021-02-01 15:33:01 +0900

    mysql styledcomponents installed

--- fr/src/App.js
+++ fr/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 export default function App() {
   const [greeting, setGreeting] = useState();
   useEffect(() => {
---
+++ fr/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {
+  useState,
+  useContext,
+  useEffect,
+  useCallback,
+  useMemo,
+} from "react";
+
+export default function Header() {
+  return <div>header</div>;
+}
